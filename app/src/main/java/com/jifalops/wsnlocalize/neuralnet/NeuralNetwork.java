3bb656c71740cfefe1263e91f6195bbfb1117528
Jacob Phillips <jphilli85@gmail.com>
2015-08-31 18:49:37 -0400

    going to refactor NeuralNetwork.java

--- app/src/main/java/com/jifalops/wsnlocalize/neuralnet/NeuralNetwork.java
+++ app/src/main/java/com/jifalops/wsnlocalize/neuralnet/NeuralNetwork.java
@@ -12,13 +12,20 @@ import java.util.List;
 public abstract class NeuralNetwork {
     protected final double[][] population;
     protected final double[] errors;
-    protected final MlpWeightMetrics metrics;
+    protected final MlpWeightMetrics weightMetrics;
     protected TrainingStatus status;
 
-    public NeuralNetwork(double[][] population, MlpWeightMetrics metrics) {
+    private TrainingMetrics trainingMetrics;
+    private ScaleMetrics scaleMetrics;
+
+    public NeuralNetwork(double[][] population, MlpWeightMetrics weightMetrics) {
         this.population = population;
-        this.metrics = metrics;
+        this.weightMetrics = weightMetrics;
         this.errors = new double[population.length];
+    }
+
+    public double[] getGlobalBest() {
+        return status.getBest();
     }
 
     public double test(double[][] data) {
@@ -26,46 +33,63 @@ public abstract class NeuralNetwork {
         return calcError(status.getBest(), data);
     }
 
-    public double[] getGlobalBest() {
+
+    protected abstract void trainSampleBySample(double[][] samples);
+
+    public double[] trainSampleBySample(double[][] samples, ScaleMetrics metrics,
+                                        TerminationConditions conditions) {
+        scaleMetrics = metrics;
+
+        trainingMetrics = new TrainingMetrics(samples);
+        samples = randomize(scale(samples, weightMetrics, metrics));
+        status = new TrainingStatus(weightMetrics, conditions);
+        double stdDev;
+        int generation = 0;
+        do {
+            trainSampleBySample(samples);
+            stdDev = Stats.stdDev(errors);
+            generation++;
+        } while (!status.isComplete(generation, stdDev));
         return status.getBest();
     }
 
+
     /**
-     * Calculate outputs using the current weights and the given inputs.
+     * Calculate outputs using the given weights and inputs.
      * The entire sample can be used (inputs + outputs) as long as the inputs
      * occupy the lower indexes (only the inputs will be used).
      */
-    protected double[] calcOutputs(double[] weights, double[] sample) {
-        double[] outputs = new double[metrics.numOutputs];
-        double[] gamma = new double[metrics.numHidden];
-        double[] z = new double[metrics.numHidden];
+    protected double[] calcOutputs(double[] weights, double[] inputsOrSample) {
+        double[] outputs = new double[weightMetrics.numOutputs];
+        double[] gamma = new double[weightMetrics.numHidden];
+        double[] z = new double[weightMetrics.numHidden];
 
         int start;
         // Weights for connections between input and hidden neurons.
-        for (int i = 0; i < metrics.numInputs; i++) {
-            start = i * metrics.numHidden;
-            for (int j = 0; j < metrics.numHidden; j++) {
-                gamma[j] += weights[start + j] * sample[i];
+        for (int i = 0; i < weightMetrics.numInputs; i++) {
+            start = i * weightMetrics.numHidden;
+            for (int j = 0; j < weightMetrics.numHidden; j++) {
+                gamma[j] += weights[start + j] * inputsOrSample[i];
             }
         }
 
-        for (int j = 0; j < metrics.numHidden; j++) {
+        for (int j = 0; j < weightMetrics.numHidden; j++) {
             // Weights for the biases of hidden neurons.
-            gamma[j] += weights[metrics.hiddenBiasesStart + j];
+            gamma[j] += weights[weightMetrics.hiddenBiasesStart + j];
 
             // Sigmoid activation
             z[j] = 1 / (1 + Math.exp(-gamma[j]));
 
             // Weights for connections between hidden and output neurons.
-            start = metrics.hiddenToOutputStart + j * metrics.numOutputs;
-            for (int k = 0; k < metrics.numOutputs; k++) {
+            start = weightMetrics.hiddenToOutputStart + j * weightMetrics.numOutputs;
+            for (int k = 0; k < weightMetrics.numOutputs; k++) {
                 outputs[k] += weights[start + k] * z[j];
             }
         }
 
         // Weights for the biases of output neurons.
-        for (int k = 0; k < metrics.numOutputs; k++) {
-            outputs[k] += weights[metrics.outputBiasesStart + k];
+        for (int k = 0; k < weightMetrics.numOutputs; k++) {
+            outputs[k] += weights[weightMetrics.outputBiasesStart + k];
         }
 
         return outputs;
@@ -79,43 +103,32 @@ public abstract class NeuralNetwork {
         double[] outputs;
         for (int i = 0; i < samples.length; i++) {
             outputs = calcOutputs(weights, samples[i]);
-            for (int j = 0; j < metrics.numOutputs; j++) {
-                error += Math.pow(outputs[j] - samples[i][metrics.numInputs + j], 2) / 2;
+            for (int j = 0; j < weightMetrics.numOutputs; j++) {
+                error += Math.pow(outputs[j] - samples[i][weightMetrics.numInputs + j], 2) / 2;
             }
         }
         return error / samples.length;
     }
 
 
-    protected abstract void trainSampleBySample(double[][] samples);
 
-    public double[] trainSampleBySample(double[][] samples, TerminationConditions conditions) {
-        status = new TrainingStatus(metrics, conditions);
-        double stdDev;
-        int generation = 0;
-        do {
-            trainSampleBySample(samples);
-            stdDev = Stats.stdDev(errors);
-            generation++;
-        } while (!status.isComplete(generation, stdDev));
-        return status.getBest();
+    public static double[][] initPop(int popSize, MlpWeightMetrics metrics) {
+        double[][] pop = new double[popSize][metrics.numWeights];
+        for (int i = 0; i < popSize; i++) {
+            for (int j = 0; j < metrics.numWeights; j++) {
+                pop[i][j] = (Math.random() - 0.5) * 2; // [-1, 1)
+            }
+        }
+        return pop;
     }
 
-
-
-
-
-    public static double[][] scale(double[][] data, SampleMetrics metrics) {
-        return scale(data, metrics, -1, 1, 0, 1);
-    }
-    public static double[][] scale(double[][] data, SampleMetrics metrics, double inputMin,
-                            double inputMax, double outputMin, double outputMax) {
+    public static double[][] scale(double[][] data, SampleMetrics metrics, ScaleMetrics s) {
         int samples = data.length;
         int cols = data[0].length;
         double[][] scaled = new double[samples][cols];
         double min, max, diff,
-                inDiff = inputMax - inputMin,
-                outDiff = outputMax - outputMin;
+                inDiff = s.inputMax - s.inputMin,
+                outDiff = s.outputMax - s.outputMin;
         for (int i = 0; i < metrics.numInputs; i++) {
             min = Double.MAX_VALUE;
             max = Double.MIN_VALUE;
@@ -125,7 +138,7 @@ public abstract class NeuralNetwork {
             }
             diff = max - min;
             for (int j = 0; j < samples; j++) {
-                scaled[j][i] = inputMin + (data[j][i] - min) * inDiff / diff;
+                scaled[j][i] = s.inputMin + (data[j][i] - min) * inDiff / diff;
             }
         }
 
@@ -138,7 +151,7 @@ public abstract class NeuralNetwork {
             }
             diff = max - min;
             for (int j = 0; j < samples; j++) {
-                scaled[j][i] = outputMin + (data[j][i] - min) * outDiff / diff;
+                scaled[j][i] = s.outputMin + (data[j][i] - min) * outDiff / diff;
             }
         }
 
--- app/src/main/java/com/jifalops/wsnlocalize/neuralnet/TerminationConditions.java
+++ app/src/main/java/com/jifalops/wsnlocalize/neuralnet/TerminationConditions.java
@@ -6,7 +6,7 @@ package com.jifalops.wsnlocalize.neuralnet;
 public class TerminationConditions {
     int maxGenerations = 200;
     double minError = 1e-3;
-    double minStdDev = 1e-4;
-    double minImprovement = 1e-4;
-    int minImprovementGenerations = 10;
+    double minStdDev = minError / 10;
+    double minImprovement = minError / 10;
+    int minImprovementGenerations = 5;
 }
