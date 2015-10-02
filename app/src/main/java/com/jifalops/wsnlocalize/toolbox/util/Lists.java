a8ce28c5f25fc62d48bd3c4a2273b55f15047001
Jacob Phillips <jphilli85@gmail.com>
2015-10-02 17:32:19 -0400

    Added toPrimitive method (was in Arrays class)

--- app/src/main/java/com/jifalops/wsnlocalize/toolbox/util/Lists.java
+++ app/src/main/java/com/jifalops/wsnlocalize/toolbox/util/Lists.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Lists {
     private Lists() {}
 
-    public static List<String> toString(List<?> objects) {
+    public static List<String> toStrings(List<?> objects) {
         List<String> strings = new ArrayList<>();
         for (Object obj : objects) {
             strings.add(obj.toString());
@@ -17,11 +17,20 @@ public class Lists {
         return strings;
     }
 
-    public static int[] toPrimitive(Integer[] ints) {
-        int len = ints.length;
-        int[] a = new int[len];
+    public static double[] toPrimitive(List<Double> list) {
+        int len = list.size();
+        double[] a = new double[len];
         for (int i = 0; i < len; ++i) {
-            a[i] = ints[i];
+            a[i] = list.get(i);
+        }
+        return a;
+    }
+
+    public static double[][] toPrimitive2D(List<double[]> list) {
+        int len = list.size();
+        double[][] a = new double[len][];
+        for (int i = 0; i < len; ++i) {
+            a[i] = list.get(i);
         }
         return a;
     }
